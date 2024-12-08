c5a2147ed646809f626572b8b4e1af70e8c88c6b
_Cam <bdaace09@gmail.com>
2024-12-08 17:24:14 -0500

    Add solution 3 part 2

---
+++ 2024/3pt2.py
@@ -0,0 +1,75 @@
+raw = open("main.txt","r").readline()
+numbers = "0123456789"
+cur_progress = 0
+progress = "mul("
+cur_num = []
+numbers_found = False
+num1 = 0
+num2 = 0
+comma_found = False
+final = 0
+enabled = True
+
+def dosearch(j,letter,raw,prev):
+    if letter == "d":
+        if raw[j+1] == "o":
+            if raw[j+2] == "(":
+                if raw[j+3] == ")":
+                    return True
+            elif raw[j+2] == "n":
+                if raw[j+3] == "'":
+                    if raw[j+4] == "t":
+                        if raw[j+5] == "(":
+                            if raw[j+6] == ")":
+                                return False
+    return prev
+
+
+for i,letter in enumerate(raw):
+    enabled = dosearch(i,letter,raw,enabled)
+    if enabled == False:
+        num1=0
+        num2=0
+        cur_progress=0
+        comma_found = False
+        numbers_found = False
+        cur_num = []
+        continue
+    if cur_progress < 4:
+        needed = progress[cur_progress]
+        if letter == needed:
+            cur_progress+=1
+        else:
+            cur_progress = 0
+    else:
+        if letter in numbers:
+            cur_num.append(letter)
+            numbers_found = True
+            continue
+        elif letter == ",":
+            if numbers_found == True and comma_found == False:
+                num1 = int(''.join(cur_num))
+                cur_num = []
+                comma_found = True
+                numbers_found = False
+                continue
+        elif letter == ")":
+            if comma_found == True and numbers_found == True:
+                num2 = int(''.join(cur_num))
+                final += num1*num2
+                num1=0
+                num2=0
+                cur_progress=0
+                comma_found = False
+                numbers_found = False
+                cur_num = []
+                continue
+        else:
+            num1=0
+            num2=0
+            cur_progress=0
+            comma_found = False
+            numbers_found = False
+            cur_num = []
+            continue
+print(final)
