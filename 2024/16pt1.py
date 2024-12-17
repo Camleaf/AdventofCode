d7fc9285e912306e208f9a769d7438cc038755fa
Camleaf <bdaace09@gmail.com>
2024-12-16 19:07:17 -0500

    test

--- 2024/16pt1.py
+++ 2024/16pt1.py
@@ -1,8 +1,8 @@
 from collections import deque
 import sys
 
-from AdventofCode.utilities.general import data_collect
-from AdventofCode.utilities.grid import CLOCKWISE_DIRS,COUNTERCLOCKWISE_DIRS,Grid
+from general import data_collect
+from grid import CLOCKWISE_DIRS,COUNTERCLOCKWISE_DIRS,Grid
 
 raw = data_collect(2024,16)
 grid = Grid(raw)
--- utilities/general.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from abc import ABC, abstractmethod
-
-
-def data_collect(year:int, day:int):
-    with open(f"https://adventofcode.com/{year}/day/{day}/input","r") as file:
-         return file.read()
