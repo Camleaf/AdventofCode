95083e55c904b62ec24b34dffac2e7711c744575
Camleaf <bdaace09@gmail.com>
2024-12-16 20:05:57 -0500

    day 16 completed

---
+++ 2024/16pt2.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+from grid import CLOCKWISE_DIRS,Grid
+
+with open("main.in","r") as file:
+    raw = file.read()
+    grid = Grid(raw)
+    grid.print()
+
+for row, col in grid.nodes_indicies():
+    if grid.get_node_val(row, col) == "S":
+        start = (row, col)
+
+dir = 1
+lowest = {}
+
+lowest_cost = {}
+queue = deque(())
+queue.append((start[0],start[1],dir,0,[]))
+def is_valid(row,col,grid):
+    return grid.get_node_val(row,col) != "#"
+while queue:
+    cur_y, cur_x, cur_dir, cur_score,history = queue.popleft()
+    lowest_cost[(cur_y, cur_x, cur_dir)] = min(cur_score,lowest_cost.get((cur_y, cur_x, cur_dir), float("inf")))
+    if grid.get_node_val(cur_y,cur_x) == "E":
+        if cur_score not in lowest.keys():
+            lowest[cur_score] = []
+        lowest[cur_score] += history
+    
+    y_straight = cur_y + CLOCKWISE_DIRS[cur_dir][0]
+    x_straight = cur_x + CLOCKWISE_DIRS[cur_dir][1]
+    next_score_straight = cur_score + 1
+    if is_valid(y_straight,x_straight,grid) and next_score_straight <= lowest_cost.get((y_straight, x_straight, cur_dir), float("inf")):
+        lowest_cost[(y_straight, x_straight, cur_dir)] = next_score_straight
+        queue.append((y_straight, x_straight, cur_dir, next_score_straight, tuple([x for x in history] + [(cur_y,cur_x)])))
+
+    for turn_indicator in ((cur_dir +1) % 4,(cur_dir -1) % 4):
+        y_turn = cur_y + CLOCKWISE_DIRS[turn_indicator][0]
+        x_turn = cur_x + CLOCKWISE_DIRS[turn_indicator][1]
+        next_score_turn = cur_score + 1001
+        if is_valid(y_turn,x_turn,grid) and next_score_turn <= lowest_cost.get((y_turn,x_turn, turn_indicator), float("inf")):
+            lowest_cost[(y_turn,x_turn, turn_indicator)] = next_score_turn
+            queue.append((y_turn,x_turn, turn_indicator, next_score_turn, [x for x in history] + [(cur_y,cur_x)]))
+
+print(len(set(lowest[min(lowest.keys())])) + 1)
