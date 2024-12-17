1c95b991dcea02890781df096be1dafeaf1cd4d2
Camleaf <bdaace09@gmail.com>
2024-12-16 19:02:57 -0500

    rtest

--- 2024/16pt1.py
+++ 2024/16pt1.py
@@ -1,7 +1,8 @@
 from collections import deque
+import sys
 
-from AdventofCode.utils.general import data_collect
-from AdventofCode.utils.grid import CLOCKWISE_DIRS,COUNTERCLOCKWISE_DIRS,Grid
+from AdventofCode.utilities.general import data_collect
+from AdventofCode.utilities.grid import CLOCKWISE_DIRS,COUNTERCLOCKWISE_DIRS,Grid
 
 raw = data_collect(2024,16)
 grid = Grid(raw)
