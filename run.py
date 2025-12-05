9ae06f5967409377627f5ddd0b368dcb346216eb
Camleaf <bdaace09@gmail.com>
2025-12-05 08:22:41 -0500

    brhosdoifosijd

--- run.py
+++ run.py
@@ -5,10 +5,8 @@
 import os, time
 from types import ModuleType
 import importlib
-clear = lambda : os.system('clear')
 
 if __name__ == "__main__":
-    clear()
     day:str = str(input("Enter Day:\n >"))
 
     if (len(day) == 0 or not day.isnumeric()):
@@ -28,19 +26,22 @@ if __name__ == "__main__":
     part1 = getattr(dayImport,"part1")
     part2 = getattr(dayImport,"part2")
 
-    startTime = time.time()
+    
 
     print("Part 1 answer: ",end="")
-    print(part1())
-
+    startTime = time.time()
+    p1 = part1()
     part1Time = time.time() - startTime
-    startTime = time.time()
+    print(p1)
+
+
 
 
     print("Part 1 answer: ",end="")
-
-    print(part2())
+    startTime = time.time()
+    p2 = part2()
     part2Time = time.time()-startTime
+    print(p2)
 
 
     print("\nPart 1 time:",str(round(1000*(part1Time),4)) + "ms")
